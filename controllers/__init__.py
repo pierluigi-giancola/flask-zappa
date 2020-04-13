58c9440c85f7961d0a88d6599a4d2e1277616fb2
pierluigigiancola <pierluigi.giancola@gmail.com>
2020-04-13 23:46:47 +0200

    Return total length of delivery

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -4,7 +4,7 @@ import json
 def handle_error(fn):
     def decorator(*args, **kwargs):
         try:
-            return jsonify(json.loads(fn(*args, **kwargs).to_json())), 200
+            return jsonify(json.loads(fn(*args, **kwargs))), 200
         except Exception as e:
             return jsonify(str(e)), 500
     return decorator
